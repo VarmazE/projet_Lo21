31cbdba220da25646f3cb1c3dcc0634357b5e5ce
VarmazE <154643931+VarmazE@users.noreply.github.com>
2023-12-23 16:54:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 
     Regle* regle_N1=creer_Regle_Vide();
     regle_N1= Ajout_propostion_regle(regle_N1,"GCF");
-    printf("la branche evren");
+    printf("la cececee evren");
 
     
     printf("%s",regle_N1->conclusion);
